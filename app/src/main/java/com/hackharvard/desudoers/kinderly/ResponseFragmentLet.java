7a00e39c956c3cc29e96dead94be431ec82e5826
Sarathkrishnan Ramesh <sarathkrishnan99@gmail.com>
2018-12-24 14:04:11 +0530

    Add Fragments to Let Activity

---
+++ app/src/main/java/com/hackharvard/desudoers/kinderly/ResponseFragmentLet.java
@@ -0,0 +1,18 @@
+package com.hackharvard.desudoers.kinderly;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class ResponseFragmentLet extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstancestate) {
+        return inflater.inflate(R.layout.fragment_responses_let,container,false);
+
+        // TODO: Response page for Let
+    }
+}
