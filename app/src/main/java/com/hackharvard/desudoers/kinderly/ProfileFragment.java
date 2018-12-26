8e6eef721470fc62fdd2477059749de8b99ea611
Sarathkrishnan Ramesh <sarathkrishnan99@gmail.com>
2018-12-27 02:57:44 +0530

    Add image selection in profile
    - Can select profile picture from gallery or can capture an image from camera
    - Will create an app folder which holds all media files in it

--- app/src/main/java/com/hackharvard/desudoers/kinderly/ProfileFragment.java
+++ app/src/main/java/com/hackharvard/desudoers/kinderly/ProfileFragment.java
@@ -1,18 +1,130 @@
 package com.hackharvard.desudoers.kinderly;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 
-public class ProfileFragment extends Fragment {
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+
+import static android.app.Activity.RESULT_OK;
+
+public class ProfileFragment extends Fragment implements View.OnClickListener{
+    Button changePicGallery;
+    Button changePicCamera;
+    ImageView proPic;
+    TextView name;
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstancestate) {
-        return inflater.inflate(R.layout.fragment_profile,container,false);
-
-        // TODO: Profile of User
+        View view =  inflater.inflate(R.layout.fragment_profile,container,false);
+        changePicGallery = view.findViewById(R.id.change_pro_pic_gallery);
+        changePicCamera = view.findViewById(R.id.change_pro_pic_camera);
+        proPic = view.findViewById(R.id.profile_photo);
+        changePicGallery.setOnClickListener(this);
+        changePicCamera.setOnClickListener(this);
+        name = view.findViewById(R.id.user_name);
+        return view;
     }
+
+    @Override
+    public void onClick(View view)
+    {
+        switch(view.getId())
+        {
+            case R.id.change_pro_pic_gallery:
+                Intent pickPhoto = new Intent(Intent.ACTION_PICK,
+                        android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                startActivityForResult(pickPhoto , 1);
+                break;
+
+            case R.id.change_pro_pic_camera:
+                Intent takePicture = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                startActivityForResult(takePicture, 0);
+        }
+
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent imageReturnedIntent) {
+        super.onActivityResult(requestCode, resultCode, imageReturnedIntent);
+        switch(requestCode) {
+            case 0:
+                if(resultCode == RESULT_OK){
+
+                    /* Working
+                    Bundle extras = imageReturnedIntent.getExtras();
+                    Bitmap imageBitmap = (Bitmap) extras.get("data");
+                    proPic.setImageBitmap(imageBitmap);
+                    */
+
+                    try {
+                        Bitmap bitmap = (Bitmap) imageReturnedIntent.getExtras().get("data");
+                        ByteArrayOutputStream bytes = new ByteArrayOutputStream();
+                        bitmap.compress(Bitmap.CompressFormat.JPEG, 50, bytes);
+
+
+                        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss", Locale.getDefault()).format(new Date());
+                        File appDirectory = new File(Environment.getExternalStorageDirectory()+"/"+getString(R.string.app_name));
+                        if(!(appDirectory.exists() && appDirectory.isDirectory()))
+                        {
+                            try{
+                                appDirectory.mkdir();
+                            }
+                            catch (SecurityException e) {
+                                e.printStackTrace();
+                            }
+                        }
+                        File destination = new File(Environment.getExternalStorageDirectory() + "/" +
+                                getString(R.string.app_name), "IMG-" + timeStamp + ".jpg");
+                        FileOutputStream fo;
+                        try {
+                            destination.createNewFile();
+                            fo = new FileOutputStream(destination);
+                            fo.write(bytes.toByteArray());
+                            fo.flush();
+                            fo.close();
+                        } catch (FileNotFoundException e) {
+                            e.printStackTrace();
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
+
+                        String imgPath = destination.getAbsolutePath();
+                        Uri selectedImage = Uri.parse(imgPath);
+                        proPic.setImageURI(selectedImage);
+
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+                }
+
+                break;
+            case 1:
+                if(resultCode == RESULT_OK){
+                    Uri selectedImage = imageReturnedIntent.getData();
+                    proPic.setImageURI(selectedImage);
+                }
+                break;
+        }
+    }
+
 }
