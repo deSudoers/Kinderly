4f08b69d77a8a1bccf9c6e0faa1579ffa2691021
Sarathkrishnan Ramesh <sarathkrishnan99@gmail.com>
2018-12-27 17:20:28 +0530

    Integrate Profile with server

--- app/src/main/java/com/hackharvard/desudoers/kinderly/ProfileFragment.java
+++ app/src/main/java/com/hackharvard/desudoers/kinderly/ProfileFragment.java
@@ -1,8 +1,12 @@
 package com.hackharvard.desudoers.kinderly;
 
+import android.app.ProgressDialog;
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.graphics.Bitmap;
 import android.net.Uri;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.os.Environment;
 import android.provider.MediaStore;
@@ -16,11 +20,21 @@ import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
 import java.io.ByteArrayOutputStream;
+import java.io.DataOutputStream;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.Locale;
@@ -31,7 +45,8 @@ public class ProfileFragment extends Fragment implements View.OnClickListener{
     Button changePicGallery;
     Button changePicCamera;
     ImageView proPic;
-    TextView name;
+    TextView name,age,mobileNum;
+    ProgressDialog pd;
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstancestate) {
@@ -42,7 +57,34 @@ public class ProfileFragment extends Fragment implements View.OnClickListener{
         changePicGallery.setOnClickListener(this);
         changePicCamera.setOnClickListener(this);
         name = view.findViewById(R.id.user_name);
+        age = view.findViewById(R.id.age);
+        mobileNum = view.findViewById(R.id.phone_number);
+        SharedPreferences sp = getActivity().getSharedPreferences("profile",Context.MODE_PRIVATE);
+        String details = sp.getString("data",null);
+        if(details == null)
+            new JsonTask(getString(R.string.url)+"profile").execute((Void)null);
+        else
+            updateProfileInfo(details);
         return view;
+    }
+
+    public void updateProfileInfo(String details)
+    {
+        String userName = null;
+        String userMobile = null;
+        try {
+            JSONObject profileData = new JSONObject(details);
+            userName = profileData.getJSONObject("user").getString("first_name");
+            userName += " " + profileData.getJSONObject("user").getString("last_name");
+            userMobile = profileData.getJSONObject("user").getString("mobile");
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        finally {
+            name.setText(userName);
+            mobileNum.setText(userMobile);
+            age.setText("180");
+        }
     }
 
     @Override
@@ -59,6 +101,7 @@ public class ProfileFragment extends Fragment implements View.OnClickListener{
             case R.id.change_pro_pic_camera:
                 Intent takePicture = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
                 startActivityForResult(takePicture, 0);
+                break;
         }
 
     }
@@ -69,12 +112,6 @@ public class ProfileFragment extends Fragment implements View.OnClickListener{
         switch(requestCode) {
             case 0:
                 if(resultCode == RESULT_OK){
-
-                    /* Working
-                    Bundle extras = imageReturnedIntent.getExtras();
-                    Bitmap imageBitmap = (Bitmap) extras.get("data");
-                    proPic.setImageBitmap(imageBitmap);
-                    */
 
                     try {
                         Bitmap bitmap = (Bitmap) imageReturnedIntent.getExtras().get("data");
@@ -127,4 +164,72 @@ public class ProfileFragment extends Fragment implements View.OnClickListener{
         }
     }
 
+
+    private class JsonTask extends AsyncTask<Void, Void, String> {
+
+        private final String profileUrl;
+
+        JsonTask(String s)
+        {
+            profileUrl = s;
+        }
+
+        protected void onPreExecute() {
+            super.onPreExecute();
+
+            pd = new ProgressDialog(getActivity());
+            pd.setMessage("Please wait");
+            pd.setCancelable(false);
+            pd.show();
+        }
+
+        @Override
+        protected String doInBackground(Void... params) {
+            // TODO: attempt authentication against a network service.
+            String data = "Error";
+            try{
+                HttpURLConnection httpURLConnection = null;
+                try {
+                    httpURLConnection = (HttpURLConnection) new URL(profileUrl).openConnection();
+                    httpURLConnection.setRequestMethod("GET");
+                    SharedPreferences sp = getActivity().getSharedPreferences("login",Context.MODE_PRIVATE);
+                    httpURLConnection.addRequestProperty("cookie", sp.getString("token2", ""));
+                    httpURLConnection.addRequestProperty("cookie", sp.getString("token", ""));
+                    httpURLConnection.setDoOutput(false);
+                    httpURLConnection.setDoInput(true);
+                    String line;
+                    data = "";
+                    BufferedReader br = new BufferedReader(new InputStreamReader(httpURLConnection.getInputStream()));
+                    while ((line = br.readLine()) != null) {
+                        data += line;
+                    }
+
+                } catch (Exception e) {
+                    e.printStackTrace();
+                    Log.e("login", e.toString());
+                }
+                finally {
+                    if (httpURLConnection != null) {
+                        httpURLConnection.disconnect();
+                    }
+                }
+            }
+            catch (Exception e){
+                e.printStackTrace();
+            }
+
+            return data;
+        }
+
+        @Override
+        protected void onPostExecute(String result) {
+            super.onPostExecute(result);
+            if (pd.isShowing()){
+                pd.dismiss();
+            }
+            updateProfileInfo(result);
+            SharedPreferences sp = getActivity().getSharedPreferences("profile",Context.MODE_PRIVATE);
+            sp.edit().putString("data",result).apply();
+        }
+    }
 }
